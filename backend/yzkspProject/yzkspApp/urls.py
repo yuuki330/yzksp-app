80fd8c99a8116efbcfaf8cfcecb09166157d5657
yuuki330 <katsuyu107@gmail.com>
2024-09-04 15:53:41 +0900

    URL設定の変更

--- backend/yzkspProject/yzkspApp/urls.py
+++ backend/yzkspProject/yzkspApp/urls.py
@@ -10,17 +10,12 @@ router.register(r'events', EventViewSet)
 router.register(r'participants', ParticipantViewSet)
 router.register(r'attendances', AttendanceViewSet, basename='attendance')
 
-api_urlpatterns = [
+urlpatterns = [
     path('', include(router.urls)),
     path('login/', CustomLoginView.as_view(), name='custom_login'),
     path('logout/', CustomLogoutView.as_view(), name='logout'),
     path('register/', RegisterView.as_view(), name='register'),
-    path('get-username/', views.get_username, name='get_username'),
+    path('get-username/', get_username, name='get_username'),
     path('current-user/', current_user_view, name='current_user'),
     path('events/<int:event_pk>/attendances/', AttendanceViewSet.as_view({'get': 'list', 'post': 'create'}), name='event-attendances'),
-]
-
-urlpatterns = [
-    path('', RedirectView.as_view(url='/api/', permanent=False)),
-    path('api/', include(api_urlpatterns)),
 ]
